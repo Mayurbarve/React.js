763bdc90f7af219f1c7ec4868478ee403d45b4d3
Mayura Dudhbarve <mayurbarve04@gmail.com>
2024-03-26 13:45:21 +0530

    useEffec index and render file

---
+++ useEffect/UseEffect.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react'
+
+const UseEffect = () => {
+
+    const [clicked, setclicked] = useState();
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        console.log('We are in Use state')
+    },[clicked])
+
+    return (
+        <div>
+            <button onClick={() => setclicked('Follow')}>
+                Follow</button>
+            <br />
+            <hr />
+            <button onClick={() => setclicked('To')}>
+                to</button>
+            <br />
+            <hr />
+            <button onClick={() => setclicked('mayura')}>
+                mayura</button>
+            <h1>{clicked}</h1>
+            <button onClick={() => {setCount(count + 1)}}>Increment</button>
+            <h1>{count}</h1>
+        </div>
+    )
+}
+
+export default UseEffect
---
+++ useEffect/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import UseEffect from './UseEffect.js' 
+
+import UseEffect from './UseEffectCleanup'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <UseEffect />
+  </React.StrictMode>
+); 
